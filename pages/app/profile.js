79f9db0aae5bfd7fa68f88720fb7eae883f7da0e
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-09 22:47:56 +0700

    Add: profile page

---
+++ pages/app/profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  AppContainer,
+  AppTitle,
+} from "../../styles/styled-components/app/appComponents";
+
+const profile = () => {
+  return (
+    <AppContainer>
+      <AppTitle>Profile</AppTitle>
+    </AppContainer>
+  );
+};
+
+export default profile;
