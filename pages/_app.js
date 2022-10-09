a15e136744893b3d50be6b4c3b7b263eb1cdbe20
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-09 17:37:05 +0700

    Add: context, account state and passing path props

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import AppContext from "../context/AppContext";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  console.log(router.locale);
+  const basePath = router.pathname.split("/");
+  const [account, setAccount] = useState([]);
   return (
-    <Layout path={router.pathname}>
-      <Component {...pageProps} />
-    </Layout>
+    <AppContext.Provider value={{ user: account[0], setAccount: setAccount }}>
+      <Layout path={basePath[1]}>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContext.Provider>
   );
 }
 
