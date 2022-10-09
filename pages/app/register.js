0b898839cc4cd6315f79ea71d398d4a6bf1863ff
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-09 22:48:08 +0700

    Add: register session page

---
+++ pages/app/register.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  AppContainer,
+  AppTitle,
+} from "../../styles/styled-components/app/appComponents";
+
+const register = () => {
+  return (
+    <AppContainer>
+      <AppTitle>Register to Session</AppTitle>
+    </AppContainer>
+  );
+};
+
+export default register;
