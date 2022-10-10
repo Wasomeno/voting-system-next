5cb9db4fd25e7837690fd0e2c5781bcc3a498aaf
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-10 14:50:10 +0700

    Add: profile page ui

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -2,12 +2,39 @@ import React from "react";
 import {
   AppContainer,
   AppTitle,
+  Table,
+  TableData,
+  TableHead,
+  Text,
 } from "../../styles/styled-components/app/appComponents";
 
 const profile = () => {
   return (
     <AppContainer>
       <AppTitle>Profile</AppTitle>
+      <Text>Your history</Text>
+      <Table>
+        <tr>
+          <TableHead>
+            <Text>Session Id</Text>
+          </TableHead>
+          <TableHead>
+            <Text>Session Title</Text>
+          </TableHead>
+          <TableHead>
+            <Text>Voted For</Text>
+          </TableHead>
+          <TableHead>
+            <Text>Time Stamp</Text>
+          </TableHead>
+        </tr>
+        <tr>
+          <TableData>1313131</TableData>
+          <TableData>Lorem Ipsum</TableData>
+          <TableData>Candidate 1</TableData>
+          <TableData>03/04/2022</TableData>
+        </tr>
+      </Table>
     </AppContainer>
   );
 };
