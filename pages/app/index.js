8e332f8c21ac0e9e496fa03e5ef313a19dfa44f9
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-11 22:58:45 +0700

    Add: simple search session handler

--- pages/app/index.js
+++ pages/app/index.js
@@ -1,15 +1,76 @@
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import AppContext from "../../context/AppContext";
+import useContract from "../../hooks/useContract";
 import {
   AppContainer,
   AppTitle,
-} from "../../styles/styled-components/app/appHomeComponents";
+  Button,
+  Form,
+  Input,
+  Section,
+  Submit,
+  Text,
+} from "../../styles/styled-components/app/appComponents";
 
 export default function AppHome() {
+  const [sessionId, setSessionId] = useState("");
+  const [result, setResult] = useState(false);
+  const votingContract = useContract("voting");
+  const toast = useContext(AppContext).toast;
+
+  const findSession = async () => {
+    const result = await votingContract.checkSession(sessionId);
+    toast.success("Session Found");
+    setResult(result);
+  };
+
+  const sessionInputHandler = (value) => {
+    if (value.length >= 10) return;
+    setSessionId(value);
+  };
+
+  const submitSession = (form) => {
+    form.preventDefault();
+    findSession();
+  };
+
+  useEffect(() => {}, [result]);
+
   return (
     <AppContainer>
       <AppTitle>Welcome to the Entrance</AppTitle>
       <video width="320" height="240" autoPlay muted loop>
         <source src="/vote-hero.mp4" type="video/mp4" />
       </video>
+      {!result ? (
+        <Form onSubmit={(e) => submitSession(e)}>
+          <Section>
+            <Text>Input session Id</Text>
+            <Input
+              type={"number"}
+              value={sessionId}
+              onChange={(e) => sessionInputHandler(e.target.value)}
+            />
+          </Section>
+          <Section>
+            <Submit type={"submit"} value="Find Session" />
+          </Section>
+        </Form>
+      ) : (
+        <Section>
+          <Link
+            href={{
+              pathname: "/app/sessions/[sessionId]",
+              query: { sessionId },
+            }}
+          >
+            <Button width={"10rem"} height={"3rem"}>
+              Go to Session
+            </Button>
+          </Link>
+        </Section>
+      )}
     </AppContainer>
   );
 }
