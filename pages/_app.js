d7274164c0d135b45dc02810b02a8c12e0dc50cc
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-11 22:58:12 +0700

    Add: toast hooks to AppContext

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,29 @@ import Layout from "../components/Layout";
 import "../styles/globals.css";
 import AppContext from "../context/AppContext";
 import { useState } from "react";
+import useToast, { Toast } from "../components/modal/Toast";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const basePath = router.pathname.split("/");
+  const [success, error, text, show] = useToast();
   const [account, setAccount] = useState([]);
   return (
-    <AppContext.Provider value={{ user: account[0], setAccount: setAccount }}>
+    <AppContext.Provider
+      value={{
+        user: account[0],
+        setAccount: setAccount,
+        toast: {
+          success: success,
+          error: error,
+          text: text,
+          show: show,
+        },
+      }}
+    >
       <Layout path={basePath[1]}>
         <Component {...pageProps} />
+        <Toast />
       </Layout>
     </AppContext.Provider>
   );
