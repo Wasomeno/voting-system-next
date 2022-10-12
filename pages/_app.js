44de3f741f4f77630721aed38f8a95e495af2cea
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-12 23:04:17 +0700

    Add: react query QueryClient and provider

--- pages/_app.js
+++ pages/_app.js
@@ -4,30 +4,34 @@ import "../styles/globals.css";
 import AppContext from "../context/AppContext";
 import { useState } from "react";
 import useToast, { Toast } from "../components/modal/Toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const basePath = router.pathname.split("/");
   const [success, error, text, show] = useToast();
   const [account, setAccount] = useState([]);
+  const queryClient = new QueryClient();
   return (
-    <AppContext.Provider
-      value={{
-        user: account[0],
-        setAccount: setAccount,
-        toast: {
-          success: success,
-          error: error,
-          text: text,
-          show: show,
-        },
-      }}
-    >
-      <Layout path={basePath[1]}>
-        <Component {...pageProps} />
-        <Toast />
-      </Layout>
-    </AppContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <AppContext.Provider
+        value={{
+          user: account[0],
+          setAccount: setAccount,
+          toast: {
+            success: success,
+            error: error,
+            text: text,
+            show: show,
+          },
+        }}
+      >
+        <Layout path={basePath[1]}>
+          <Component {...pageProps} />
+          <Toast />
+        </Layout>
+      </AppContext.Provider>
+    </QueryClientProvider>
   );
 }
 
