14333f5bcdd17506427e42b3f583a1ff9aa9f62d
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-13 23:43:36 +0700

    Add: getHistory function

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -1,4 +1,9 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { BigNumber } from "ethers";
+import React, { useContext } from "react";
+import AnimatedContainer from "../../components/AnimatedContainer";
+import AppContext from "../../context/AppContext";
+import useContract from "../../hooks/useContract";
 import {
   AppContainer,
   AppTitle,
@@ -9,33 +14,51 @@ import {
 } from "../../styles/styled-components/app/appComponents";
 
 const profile = () => {
+  const voterDataContract = useContract("voterData");
+  const historyFetched = useQuery(["voterHistory"], () => getHistory());
+  const user = useContext(AppContext).user;
+
+  const getHistory = async () => {
+    const history = await voterDataContract.getVoterHistory(user);
+    return history;
+  };
+
+  const toString = (value) => {
+    const string = value.toString();
+    return string;
+  };
+
   return (
-    <AppContainer>
-      <AppTitle>Profile</AppTitle>
-      <Text>Your history</Text>
-      <Table>
-        <tr>
-          <TableHead>
-            <Text>Session Id</Text>
-          </TableHead>
-          <TableHead>
-            <Text>Session Title</Text>
-          </TableHead>
-          <TableHead>
-            <Text>Voted For</Text>
-          </TableHead>
-          <TableHead>
-            <Text>Time Stamp</Text>
-          </TableHead>
-        </tr>
-        <tr>
-          <TableData>1313131</TableData>
-          <TableData>Lorem Ipsum</TableData>
-          <TableData>Candidate 1</TableData>
-          <TableData>03/04/2022</TableData>
-        </tr>
-      </Table>
-    </AppContainer>
+    <AnimatedContainer isLoading={historyFetched.isLoading}>
+      {!historyFetched.isLoading ? (
+        <>
+          <AppTitle>Profile</AppTitle>
+          <Text>Your history</Text>
+          <Table>
+            <tr>
+              <TableHead>
+                <Text>Session Id</Text>
+              </TableHead>
+              <TableHead>
+                <Text>Voted For</Text>
+              </TableHead>
+              <TableHead>
+                <Text>Time Stamp</Text>
+              </TableHead>
+            </tr>
+            <tr>
+              {historyFetched.data.map((history) => (
+                <>
+                  <TableData>{history.sessionId}</TableData>
+                  <TableData>{history.candidateId}</TableData>
+                  <TableData>{toString(history.timeStamp)}</TableData>
+                </>
+              ))}
+            </tr>
+          </Table>
+        </>
+      ) : null}
+    </AnimatedContainer>
   );
 };
 
