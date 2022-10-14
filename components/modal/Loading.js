2c9fb4761f9ea01d8aa6be6fd9254f621ed4356b
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-14 23:58:57 +0700

    Change: changed from params to context

--- components/modal/Loading.js
+++ components/modal/Loading.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text } from "../../styles/styled-components/app/appComponents";
 import {
   LoadingContainer,
   Screen,
 } from "../../styles/styled-components/modals/loadingComponents";
 import Loader from "react-spinners/PuffLoader";
+import AppContext from "../../context/AppContext";
 
-const Loading = ({ text, isLoading }) => {
+const Loading = () => {
+  const loadingContext = useContext(AppContext).loading;
   return (
     <>
-      <LoadingContainer>
-        <Text align={"center"}>{text}</Text>
-        <Loader loading={isLoading} color={"#fff"} size={75} />
+      <LoadingContainer loading={loadingContext.isLoading ? 1 : 0}>
+        <Text align={"center"}>{loadingContext.text}</Text>
+        <Loader loading={loadingContext.isLoading} color={"#fff"} size={75} />
       </LoadingContainer>
-      <Screen />
+      <Screen loading={loadingContext.isLoading ? 1 : 0} />
     </>
   );
 };
-
 export default Loading;
