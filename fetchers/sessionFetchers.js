c185853a934414fd1b44e9e39d407f299304f57f
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-14 23:59:33 +0700

    Add: moved session function to seperate file

---
+++ fetchers/sessionFetchers.js
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import AppContext from "../context/AppContext";
+import useContract from "../hooks/useContract";
+
+export const getStatus = async (session, user) => {
+  const votingContract = useContract("voting");
+  const status = await votingContract.voterVerified(session, user);
+  return status;
+};
+
+export const getDetails = async (session) => {
+  const votingContract = useContract("voting");
+  const details = await votingContract.votingDetails(session);
+  return details;
+};
+
+export const getCandidates = async (session) => {
+  const votingContract = useContract("voting");
+  const candidates = await votingContract.getCandidatesDetails(session);
+  return candidates;
+};
+
+export const getHistory = async (session) => {
+  const votingContract = useContract("voting");
+  const history = await votingContract.getHistory(session);
+  return history;
+};
