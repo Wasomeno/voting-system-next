e71cd0c06cc9ac9fa324e692ae345fda42d654a3
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-17 20:25:51 +0700

    Add: loading, toast and transaction listener

--- pages/app/create.js
+++ pages/app/create.js
@@ -1,9 +1,12 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { ethers } from "ethers";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
 import AnimatedContainer from "../../components/AnimatedContainer";
+import AppContext from "../../context/AppContext";
+import { getIsProposer } from "../../fetchers/sessionFetchers";
 import useContract from "../../hooks/useContract";
+import { useLoading, useToast } from "../../stores/stores";
 import {
-  AppContainer,
   AppTitle,
   Button,
   FlexColumn,
@@ -20,6 +23,29 @@ const create = () => {
   const sessionRef = useRef();
   const [candidates, setCandidates] = useState(["", ""]);
   const votingContract = useContract("voting");
+  const [, setLoading] = useLoading();
+  const [error, success] = useToast();
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const context = useContext(AppContext);
+  const isProposer = useQuery(["isProposer", context], () =>
+    getIsProposer(context)
+  );
+
+  const createSessionMutation = useMutation((event) => createSession(event), {
+    onMutate: () => {
+      setLoading(true);
+    },
+    onSuccess: () => {
+      setLoading(false);
+      success("Session Created");
+    },
+    onError: (data) => {
+      setLoading(false);
+      console.log(data.code);
+      error("Transaction error");
+    },
+  });
 
   const createSession = async (event) => {
     event.preventDefault();
@@ -28,7 +54,13 @@ const create = () => {
     const candidatesBytes = candidates.map((candidate) => {
       return ethers.utils.formatBytes32String(candidate);
     });
-    await votingContract.createVoting(titleBytes, session, candidatesBytes);
+    const create = await votingContract.createVoting(
+      titleBytes,
+      session,
+      candidatesBytes
+    );
+    const listener = await provider.waitForTransaction(create.hash);
+    return listener;
   };
 
   const candidatesInputHandler = (inputIndex, value) => {
@@ -52,70 +84,93 @@ const create = () => {
     setCandidates((currentCandidates) => currentCandidates.slice(0, -1));
   };
 
+  useEffect(() => {
+    if (isProposer.isFetching) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [isProposer.isLoading]);
+
+  if (isProposer.isLoading) return;
   return (
-    <AnimatedContainer>
-      <AppTitle>Create Session</AppTitle>
+    <AnimatedContainer justify={"center"} align={"center"}>
+      {isProposer.data !== false ? (
+        <>
+          <AppTitle fontSize="52px">Create Session</AppTitle>
+          <Form onSubmit={(event) => createSessionMutation.mutate(event)}>
+            <Section>
+              <Text align={"center"} margin={"5px 5px"}>
+                Session Id
+              </Text>
+              <Input ref={sessionRef} type={"number"} width={"15rem"} />
+            </Section>
 
-      <Form onSubmit={(e) => createSession(e)}>
-        <Section>
-          <Text align={"center"}>Session Id</Text>
-          <Input ref={sessionRef} type={"number"} width={"15rem"} />
-        </Section>
+            <Section>
+              <Text align={"center"} margin={"5px 5px"}>
+                Title
+              </Text>
+              <Input ref={titleRef} type={"text"} width={"15rem"} />
+            </Section>
 
-        <Section>
-          <Text align={"center"}>Title</Text>
-          <Input ref={titleRef} type={"text"} width={"15rem"} />
-        </Section>
+            <Section>
+              <Text align={"center"} margin={"5px 5px"}>
+                Candidates
+              </Text>
+              <FlexRow alignitems={"start"}>
+                <FlexColumn>
+                  {candidates.map((candidate, index) => (
+                    <Input
+                      key={index}
+                      type={"text"}
+                      width={"15rem"}
+                      value={candidate}
+                      placeholder={"Candidates " + parseInt(index + 1)}
+                      onChange={(e) =>
+                        candidatesInputHandler(index, e.target.value)
+                      }
+                    />
+                  ))}
+                </FlexColumn>
 
-        <Section>
-          <Text align={"center"}>Candidates</Text>
-          <FlexRow alignItems={"start"}>
-            <FlexColumn>
-              {candidates.map((candidate, index) => (
-                <Input
-                  key={index}
-                  type={"text"}
-                  width={"15rem"}
-                  value={candidate}
-                  placeholder={"Candidates " + parseInt(index + 1)}
-                  onChange={(e) =>
-                    candidatesInputHandler(index, e.target.value)
-                  }
-                />
-              ))}
-            </FlexColumn>
+                <FlexRow justifycontent={"center"} alignitems={"center"}>
+                  <Button
+                    type="button"
+                    width={"2rem"}
+                    height={"2rem"}
+                    onClick={candidatesDecrement}
+                  >
+                    {"<"}
+                  </Button>
+                  <Input
+                    type={"number"}
+                    value={candidates.length}
+                    readOnly
+                    width={"5rem"}
+                  />
+                  <Button
+                    type="button"
+                    width={"2rem"}
+                    height={"2rem"}
+                    onClick={candidatesIncrement}
+                  >
+                    {">"}
+                  </Button>
+                </FlexRow>
+              </FlexRow>
+            </Section>
 
-            <FlexRow justifyContent={"center"} alignItems={"center"}>
-              <Button
-                type="button"
-                width={"2rem"}
-                height={"2rem"}
-                onClick={candidatesDecrement}
-              >
-                {"<"}
-              </Button>
-              <Input
-                type={"number"}
-                value={candidates.length}
-                readOnly
-                width={"5rem"}
-              />
-              <Button
-                type="button"
-                width={"2rem"}
-                height={"2rem"}
-                onClick={candidatesIncrement}
-              >
-                {">"}
-              </Button>
-            </FlexRow>
-          </FlexRow>
-        </Section>
-
-        <Section>
-          <Submit type={"submit"} value={"Create Session"} />
-        </Section>
-      </Form>
+            <Section>
+              <Submit type={"submit"} value={"Create Session"} />
+            </Section>
+          </Form>
+        </>
+      ) : (
+        <>
+          <AppTitle>Send your Appliances to Our Team</AppTitle>
+          <Text>You need to be a verified proposer to create a session</Text>
+        </>
+      )}
     </AnimatedContainer>
   );
 };
